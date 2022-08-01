77c8c30be22b5ac7cf73ebb7b0be4dc18270389c
Ray <4958511+xuxulll@users.noreply.github.com>
2022-08-02 00:21:36 +0800

    test

--- custom_components/sharp_tv/config_flow.py
+++ custom_components/sharp_tv/config_flow.py
@@ -1,18 +1,35 @@
 """Adds config flow for Bravia TV integration."""
 from __future__ import annotations
 
-from contextlib import suppress
-import ipaddress
-import re
+import logging
 from typing import Any
+
+_LOGGER = logging.getLogger(__name__)
+
+from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.exceptions import HomeAssistantError
 
 import voluptuous as vol
 
+from contextlib import suppress
+import ipaddress
+import re
+
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_PORT
+
 from .const import DOMAIN
 
 import homeassistant.helpers.config_validation as cv
+
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required("host"): str,
+        vol.Required("port"): str,
+    }
+)
 
 def host_valid(host: str) -> bool:
     """Return True if hostname or IP address is valid."""
@@ -38,37 +55,41 @@ class SharpTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         mac = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", s).groups()[0]
         return mac
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
         """Show the setup form to the user."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
+
         errors = {}
-        if user_input is not None:
-            # Validate user input
-            host = user_input[CONF_HOST]
-            valid = host_valid(host) 
-            if valid:
-                unique_id = self.getMac(host)
-                if unique_id is not None:
-                    await self.async_set_unique_id(unique_id)
-                    self._abort_if_unique_id_configured()
-                # See next section on create entry usage
-                    return self.async_create_entry(
-                        title=host,
-                        data=user_input,
-                    )
 
-            errors["base"] = "auth_error"
+        # Validate user input
+        host = user_input[CONF_HOST]
 
-        # Specify items in the order they are to be displayed in the UI
-        data_schema = vol.Schema(
-            {
-                vol.Required(CONF_HOST, default=host): str,
-                vol.Required(CONF_PORT, default=9688): cv.port,
-            },
-        )
+        valid = host_valid(host) 
+        if valid:
+            unique_id = self.getMac(host)
+            if unique_id is not None:
+                await self.async_set_unique_id(unique_id)
+                self._abort_if_unique_id_configured()
+            # See next section on create entry usage
+                return self.async_create_entry(
+                    title=host,
+                    data=user_input,
+                )
+            else:
+                errors["base"] = "unique_id"
+                return self.async_show_form(
+                    step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+                )
+        else:
+            errors["base"] = "host_invalid"
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            )
 
-        return self.async_show_form(
-            step_id="user", data_schema=vol.Schema(data_schema), errors=errors
-        )
-
-
-
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
