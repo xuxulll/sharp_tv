72c666a7ff25462181505dde65c702ad6c09c820
Ray <xuxulll@users.noreply.github.com>
2022-08-01 21:57:41 +0800

    temp

--- custom_components/sharp_tv/config_flow.py
+++ custom_components/sharp_tv/config_flow.py
@@ -16,12 +16,7 @@ from homeassistant.data_entry_flow import FlowResult
 import homeassistant.helpers.config_validation as cv
 
 
-from .const import (
-    ATTR_CID,
-    ATTR_MAC,
-    ATTR_MODEL,
-    DOMAIN,
-)
+from .const import DOMAIN
 
 
 def host_valid(host: str) -> bool:
@@ -44,44 +39,6 @@ class SharpTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self.title = ""
         self.port: int | None = None
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry: ConfigEntry) -> SharpTVOptionsFlowHandler:
-        """Bravia TV options callback."""
-        return SharpTVOptionsFlowHandler(config_entry)
-
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Show the setup form to the user."""
-        host = user_input.get(CONF_HOST, "") if user_input else ""
-        data_schema = vol.Schema(
-            {
-                vol.Required(CONF_HOST, default=host): str,
-                vol.Required(CONF_PORT, default=9688): cv.port,
-            },
-        )
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=data_schema,
-            errors={"base": error} if error else None,
-        )
-
-
-class SharpTVOptionsFlowHandler(config_entries.OptionsFlow):
-    """Config flow options for Sharp TV."""
-
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize Sharp TV options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Manage the options."""
-        coordinator = self.hass.data[DOMAIN][self.config_entry.entry_id]
-        return await self.async_step_user()
 
     def getMac(
         self, ipaddr: str
@@ -93,26 +50,26 @@ class SharpTVOptionsFlowHandler(config_entries.OptionsFlow):
         mac = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", s).groups()[0]
         return mac
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle a flow initiated by the user."""
-        error = None
-
+    async def async_step_user(self, user_input: None):
+        """Show the setup form to the user."""
+        errors = {}
         if user_input is not None:
-            host = user_input[CONF_HOST]
-            
-            unique_id = self.getMac(host)
-            if unique_id is not None:
-                await self.async_set_unique_id(unique_id)
+            # Validate user input
+            valid = host_valid(user_input[CONF_HOST]) 
+            if valid:
+                unique_id = self.getMac(host)
+                if unique_id is not None:
+                    await self.async_set_unique_id(unique_id)
                 self._abort_if_unique_id_configured()
-
+                # See next section on create entry usage
                 return self.async_create_entry(
-                    title=host,
+                    title=user_input[CONF_HOST],
                     data=user_input,
                 )
-                
-        host = user_input.get(CONF_HOST, "") if user_input else ""
+
+            errors["base"] = "auth_error"
+
+        # Specify items in the order they are to be displayed in the UI
         data_schema = vol.Schema(
             {
                 vol.Required(CONF_HOST, default=host): str,
@@ -121,8 +78,8 @@ class SharpTVOptionsFlowHandler(config_entries.OptionsFlow):
         )
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=data_schema,
-            errors={"base": error} if error else None,
+            step_id="user", data_schema=vol.Schema(data_schema), errors=errors
         )
+
+
 
